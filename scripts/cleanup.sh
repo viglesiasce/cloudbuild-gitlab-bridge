c9bdb164a71b3cd025a7341b459a575516e53146
Vic Iglesias <viglesias@google.com>
2021-04-13 22:09:33 -0700

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Cleaning up"
-gsutil rm -r gs://vic-gcloud-dest-100-gitlab-cache/"${CUSTOM_ENV_CI_BUILD_REF}"
+gsutil rm -r gs://"${GCP_PROJECT}"-gitlab-cache/"${CUSTOM_ENV_CI_BUILD_REF}"
